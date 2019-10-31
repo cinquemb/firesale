7139059820eb36d294837e3e99e08b538ee7714e
Cinque McFarlane-Blake <1519017+cinquemb@users.noreply.github.com>
2019-10-31 17:03:41 +0000

    Update script.js

--- js/script.js
+++ js/script.js
@@ -157,6 +157,7 @@ function SF_scripts(){
 			var posting = $.post(url, term);
 			posting
 			.done(function(data){
+				console.log(data);
 				if(data=="ok"){
 					$(".alert-form-success").fadeIn(200).delay(5000).fadeOut(200);
 				}else{
